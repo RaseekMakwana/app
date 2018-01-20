38248e25871f910379c169316d96158f8620c8f5
RaseekMakwana <makwanaraseek@gmail.com>
2018-01-20 21:15:19 +0600

    d

--- views/canvas/_common_header.php
+++ views/canvas/_common_header.php
@@ -72,7 +72,7 @@
         $('#continue_facebook').hide();
         $('#continue_user_profile').show();
         FB.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, function (response) {
-            document.getElementById('fb_profile_name').innerHTML = response.name;
+            document.getElementById('fb_profile_name').innerHTML = response.last_name;
             document.getElementById('fb_profile_pic').innerHTML = "<img src='" + response.picture.data.url + "'>";
             document.getElementById('fb_login_status').value = 'connected';
         });
