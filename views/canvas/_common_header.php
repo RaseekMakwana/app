4a88b324453b61a306faaa3e48d16a4608cb568a
RaseekMakwana <makwanaraseek@gmail.com>
2018-01-21 00:59:44 +0600

    d

--- views/canvas/_common_header.php
+++ views/canvas/_common_header.php
@@ -8,7 +8,7 @@
     <div class="thumbnail" style="border-bottom: 1px solid #ccc;">
         <p style="text-align: center">
             <button onclick="login()" id="continue_facebook" class="btn btn-primary" style="display: none">Continue with Facebook</button>
-        <div id="continue_user_profile" class="btn btn-primary" style="display: none">
+        <div id="continue_user_profile" style="display: none">
             <div class="text"><i class='fa fa-facebook-official'></i> Continue with <span id="fb_profile_name"></span></div>
              
             <div id="fb_profile_pic"></div>
